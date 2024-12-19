4950eb7df4f8595882a272a9f696de32779e00d5
SuperProCoolName <minzatov.2004@mail.ru>
2024-12-19 19:34:57 +0300

    lab5

---
+++ src/recipe/recipe.controller.ts
@@ -0,0 +1,43 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Put,
+} from '@nestjs/common';
+import { RecipeService } from './recipe.service';
+import { CreateRecipeDto } from './dto/create-recipe.dto';
+import { UpdateRecipeDto } from './dto/update-recipe.dto';
+
+@Controller('recipe')
+export class RecipeController {
+  constructor(private readonly recipeService: RecipeService) {}
+
+  @Post()
+  create(@Body() createRecipeDto: CreateRecipeDto) {
+    return this.recipeService.create(createRecipeDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.recipeService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.recipeService.findOne(+id);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: string, @Body() updateRecipeDto: UpdateRecipeDto) {
+    return this.recipeService.update(+id, updateRecipeDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.recipeService.remove(+id);
+  }
+}
